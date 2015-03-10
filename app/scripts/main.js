ad37bee646c42e6857b0c2b36786e276e4a838fe
Anastasios Orfanidis <anastasios.orfanidis@wardour.co.uk>
2015-03-10 11:18:57 +0000

    Removed unsed elements script and styles

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -159,54 +159,9 @@ function monteCarlo(initialValue, annualOutflow, monthlyReturn, monthlyStdDev) {
         result[m] = v;
     }
 
-    console.log(result);
     return result;
 }
 
-/* Prompts for age and age of retirement */
-var age;
-var ageOfRetirment;
-var yearsRemaining;
-var yearsRemainingString;
-
-/* Set vars */
-var yearsInt, yearsString, futureYear, futureAge, future, futureString;
-
-/* Get current year and convert to integer */
-var currentTime = new Date().getFullYear();
-var currentYear = parseInt(currentTime);
-
-/* Set vars for years from DOM */
-var yearsInput = document.querySelector('#years');
-var yearsOutput = document.querySelector('#yearsOutput');
-var until = document.querySelector('#until');
-var yearsText = document.querySelector('#years-text');
-var timeframe = document.querySelector('#timeframe');
-var yearsFromNow = document.querySelector('.year-form-now');
-
-/* Set vars for ages */
-var futureAge = document.querySelector('#futureAge');
-
-/* Set initial values from prompts */
-var timeFrame = function(){
-  'use strict';
-  /* Prompts for age and age of retirement */
-  age = parseInt(window.prompt('Your age', 35));
-  ageOfRetirment = parseInt(window.prompt('Age of retirement', 65));
-  yearsRemaining = ageOfRetirment - age;
-  yearsRemainingString = yearsRemaining.toString();
-  window.alert(yearsRemainingString + ' ' + 'from now');
-
-  yearsOutput.textContent =  yearsRemainingString;
-  futureYear = currentYear + yearsRemaining;
-  until.textContent = futureYear;
-  future = yearsRemaining + age;
-  futureAge.textContent = future;
-
-  yearsInput.value = yearsRemaining;
-  yearsInput.setAttribute('value', yearsRemaining);
-  yearsText.setAttribute('value', yearsRemaining);
-};
 
 var addClass = function(el,className){
   if (el.classList){
@@ -216,58 +171,6 @@ var addClass = function(el,className){
     el.className += ' ' + className;
   }
 };
-
-var enabledInputs = function(){
-  var inputs = document.getElementsByTagName('input');
-  for(var i = 0; i < inputs.length; i++){
-    inputs[i].disabled = false;
-  }
-};
-
-timeframe.addEventListener('click', function(){
-  'use strict';
-  timeFrame();
-  enabledInputs();
-  addClass(yearsFromNow, 'display');
-});
-
-/* Event listener function for slider on load */
-  yearsInput.addEventListener('input', function(){
-    'use strict';
-    yearsInt = parseInt(yearsInput.value);
-    yearsString = yearsInt.toString();
-    yearsOutput.textContent =  yearsString;
-
-    futureYear = currentYear + yearsInt;
-    until.textContent = futureYear;
-
-    future = yearsInt + age;
-    futureString = future.toString();
-    futureAge.textContent = futureString;
-
-    yearsInput.setAttribute('value', yearsInt);
-    yearsText.setAttribute('value', yearsInt);
-  });
-
-  yearsText.addEventListener('keyup', function(){
-    'use strict';
-    yearsInt = parseInt(yearsText.value);
-    yearsString = yearsInt.toString();
-    yearsOutput.textContent =  yearsString;
-
-    futureYear = currentYear + yearsInt;
-    until.textContent = futureYear;
-
-    future = yearsInt + age;
-    futureString = future.toString();
-    futureAge.textContent = futureString;
-
-    
-    yearsInput.value = yearsInt;
-    yearsInput.setAttribute('value', yearsInt);
-    // return yearsInt;
-  });
-
 
 /* Set vars for years from DOM */
 // 'use strict';
@@ -483,8 +386,6 @@ var createChart = function() {
   for ( var item in data1 ) {
     numeric_array1.push( data1[ item ] );
   }
-
-  console.log(numeric_array1);
 
   var numeric_array2 = [];
   for ( var item in data2 ) {
